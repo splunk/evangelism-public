d65649fe845d16a4f4963a039b076dab3471d60c
caitlin <caitlinhalla@gmail.com>
2025-08-27 10:05:06 -0400

    Initialize Splunk RUM in React application entry point

--- manual-instrumentation/worms_in_space/assets/js/app.tsx
+++ manual-instrumentation/worms_in_space/assets/js/app.tsx
@@ -2,6 +2,10 @@ import "../css/app.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import WormsInSpace from "./WormsInSpace";
+import { initializeRUM } from "./rum";
+
+// Initialize Splunk RUM
+initializeRUM();
 
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.getElementById("WormsInSpace")
